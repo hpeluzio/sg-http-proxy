223f6efd8cc03e165693ab2cd22a67bd737554d6
Henrique Peluzio <hpeluzio@gmail.com>
2021-09-23 13:18:56 -0300

    Update .prettierrc.js and server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-var http = require("http");
-var httpProxy = require("http-proxy");
+var http = require('http');
+var httpProxy = require('http-proxy');
 
 // Reverse proxy
 var proxy = httpProxy.createProxyServer();
@@ -7,17 +7,11 @@ http
   .createServer(function (req, res) {
     var target,
       domain = req.headers.host,
-      host = domain.split(":")[0];
+      host = domain.split(':')[0];
 
-    if (host === "bolaodasorte.online")
-      target = { host: "localhost", port: "3000" };
-    if (host === "www.bolaodasorte.online")
-      target = { host: "localhost", port: "3000" };
-    if (host === "web.bolaodasorte.online")
-      target = { host: "localhost", port: "3000" };
-    if (host === "api.bolaodasorte.online")
-      target = { host: "localhost", port: "3333" };
-    else target = { host: "localhost", port: "3000" };
+    if (host === 'api.bolaodasorte.online')
+      target = { host: 'localhost', port: '3333' };
+    else target = { host: 'localhost', port: '3000' };
 
     proxy.web(req, res, {
       target: target,
