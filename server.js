6376db6c3a1a3b04ec9de6094c4cf78d16e49d1d
Henrique Peluzio <hpeluzio@gmail.com>
2021-09-23 13:12:03 -0300

    Init

---
+++ server.js
@@ -0,0 +1,26 @@
+var http = require("http");
+var httpProxy = require("http-proxy");
+
+// Reverse proxy
+var proxy = httpProxy.createProxyServer();
+http
+  .createServer(function (req, res) {
+    var target,
+      domain = req.headers.host,
+      host = domain.split(":")[0];
+
+    if (host === "bolaodasorte.online")
+      target = { host: "localhost", port: "3000" };
+    if (host === "www.bolaodasorte.online")
+      target = { host: "localhost", port: "3000" };
+    if (host === "web.bolaodasorte.online")
+      target = { host: "localhost", port: "3000" };
+    if (host === "api.bolaodasorte.online")
+      target = { host: "localhost", port: "3333" };
+    else target = { host: "localhost", port: "3000" };
+
+    proxy.web(req, res, {
+      target: target,
+    });
+  })
+  .listen(80);
